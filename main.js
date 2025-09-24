42d8069489977c4317b0a34bab469036959a21b4
Коля <135145397+Kolya4954@users.noreply.github.com>
2025-09-24 17:45:37 +0300

    finally

---
+++ main.js
@@ -0,0 +1,120 @@
+const celsius = 10;
+
+const fahrenheit = (celsius * 9/5) + 32;
+
+console.log(fahrenheit);
+
+
+
+
+const daysInMonth = 31;
+
+const hoursInMonth = daysInMonth * 24;
+
+const minutesInMonth = daysInMonth * 24 * 60;
+
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+
+
+
+
+
+
+const health = 100;
+
+const energy = 100;
+
+const minusHealth = health - 50;
+
+const minusEnergy = energy - 20;
+
+console.log(minusEnergy);
+
+console.log(minusHealth);
+
+
+
+
+
+
+
+
+const totalPrice = 100;
+
+const discount = 100 - (totalPrice * 0.10);
+
+const discountedPrice = discount;
+
+console.log(discountedPrice);
+
+
+
+
+
+
+
+
+const floatNumber = 12.78;
+
+const arounded = Math.floor(floatNumber);
+
+console.log(arounded);
+
+
+
+
+
+
+
+
+
+
+const floatString = "45.67";
+
+const parsedFloat = parseFloat(floatString);
+
+console.log(parsedFloat);
+
+
+
+
+
+
+
+const intString = "123";
+
+const parsedInt = parseInt(intString);
+
+console.log(parsedInt);
+
+
+
+
+
+
+const number = 144;
+
+const sqrtNumber = Math.sqrt(number);
+
+console.log(sqrtNumber);
+
+
+
+          
+
+
+
+
+const integer = 42;
+
+const stringNumber = "256";
+
+const convertedInt = parseInt(stringNumber);
+
+console.log(convertedInt);
+
+const convertedString = integer.toString();
+
+console.log(convertedString);
